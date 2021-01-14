8298524b3249fa7f05caf1fd44ebb141042aad13
ajay <ajayva1992@gmail.com>
2021-01-14 04:00:04 +0100

    The new repositary

---
+++ Python_Pro_Test/PY/Escape_Character.py
@@ -0,0 +1,6 @@
+split_String = "This string \n has been \n splitted"
+print(split_String)
+tabbed_String = "1\t\t2,\t3\t4\t5"
+print(tabbed_String)
+print("""The pet owener said"He is sleeping". """)
+
---
+++ Python_Pro_Test/PY/Strings.py
@@ -0,0 +1,8 @@
+print ('TODAY')
+print("Python")
+print("Python's String")
+print('We can include "Quotes" in string')
+a = "Hello "
+b = input("Please enter your name")
+c = a + b
+print(c)
